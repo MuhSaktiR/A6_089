55000fd51dd07a27fd5e5093442c5be2fa26703e
MuhSaktiR <seken876@gmail.com>
2025-01-24 15:57:53 +0700

    Membuat costum widget untuk top app bar ( pilihan opsional )

---
+++ app/src/main/java/com/example/a6_uaspam/ui/customwidget/TopAppBar.kt
@@ -0,0 +1,74 @@
+package com.example.a6_uaspam.ui.customwidget
+
+import androidx.compose.foundation.background
+import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun TopAppBar(
+    onBack: () -> Unit,
+    showBackButton: Boolean = true,
+    judul: String,
+    onRefresh: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(Color(0xFF2b95bb))
+            .padding(16.dp)
+
+    ) {
+        if (showBackButton) {
+            IconButton(
+                onClick = onBack,
+                modifier = Modifier.align(Alignment.CenterStart)
+            ) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = "Kembali",
+                    tint = Color.White,
+                    modifier = Modifier
+                        .size(30.dp)
+                )
+            }
+        }
+
+        Text(
+            text = judul,
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            modifier = Modifier.align(Alignment.Center)
+        )
+
+        // Tombol Refresh
+        IconButton(
+            onClick = onRefresh,
+            modifier = Modifier.align(Alignment.CenterEnd) // Tempatkan di kanan
+        ) {
+            Icon(
+                imageVector = Icons.Default.Refresh,
+                contentDescription = "Refresh",
+                tint = Color.White,
+                modifier = Modifier.size(30.dp) // Ukuran ikon refresh
+            )
+        }
+    }
+}
