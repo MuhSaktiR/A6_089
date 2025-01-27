259eb1510732b613f8ddb8e4fa1abaa4edcb6dd7
MuhSaktiR <seken876@gmail.com>
2025-01-27 18:24:10 +0700

    Menambahkan penyesuaian layout pada TopAppBar dengan menambah tinggi, padding, dan pengaturan warna untuk teks dan ikon

--- app/src/main/java/com/example/a6_uaspam/ui/customwidget/CostumeTopAppBar.kt
+++ app/src/main/java/com/example/a6_uaspam/ui/customwidget/CostumeTopAppBar.kt
@@ -1,6 +1,10 @@
 package com.example.a6_uaspam.ui.customwidget
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Refresh
@@ -9,42 +13,70 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CostumeTopAppBar(
     title: String,
     canNavigateBack: Boolean,
-    showRefreshButton: Boolean, // Tambahkan parameter ini
+    showRefreshButton: Boolean,
     modifier: Modifier = Modifier,
     scrollBehavior: TopAppBarScrollBehavior? = null,
     navigateUp: () -> Unit = {},
     onRefresh: () -> Unit = {},
 ) {
     CenterAlignedTopAppBar(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(100.dp) // Atur tinggi TopAppBar
+            .padding(top = 30.dp),
+
         title = { Text(
             title,
-            fontWeight = FontWeight.Bold
+            fontWeight = FontWeight.Bold,
+            color = Color.White
         ) },
         actions = {
             if (showRefreshButton) { // Tampilkan tombol refresh jika true
-                Icon(imageVector = Icons.Default.Refresh, contentDescription = "", modifier = Modifier.clickable {
-                    onRefresh()
-                })
+                Icon(
+                    imageVector = Icons.Default.Refresh,
+                    contentDescription = "",
+                    tint = Color.White, // Menetapkan warna ikon menjadi putih
+                    modifier = Modifier.clickable {
+                        onRefresh()
+                    }
+                )
             }
         },
-        modifier = modifier,
+
         scrollBehavior = scrollBehavior,
         navigationIcon = {
             if (canNavigateBack) {
-                IconButton(onClick = navigateUp) {
-                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = null)
+                IconButton(onClick = navigateUp,
+                    modifier = modifier
+                        .size(80.dp)
+                        .padding(end = 30.dp)
+                        .padding(bottom = 20.dp),) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = null,
+                        tint = Color.White,
+                    )
                 }
             }
-        }
+        },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = Color(0xFF437bba), // Menetapkan warna latar belakang menjadi biru
+            actionIconContentColor = Color.White, // Warna ikon action (refresh) menjadi putih
+            navigationIconContentColor = Color.White // Warna ikon navigasi (back) menjadi putih
+        )
+
     )
 }
